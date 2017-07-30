ec1427d15a7c3af0802e17de54cefaa248b4aec8
Xi Chen <511272827@qq.com>
2017-07-30 22:05:16 +0800

    add time: execute count
    Signed-off-by: Xi Chen <511272827@qq.com>

--- time.h
+++ time.h
@@ -7,8 +7,9 @@
 #define TIME_START(times)	\
 	{  \
 		clock_t __19870814_xixi_tag_you_not_used_start, __19870814_xixi_tag_you_not_used_finish; \
-		int __19870814_xixi_tag_you_not_used_i; \
+		int __19870814_xixi_tag_you_not_used_i, __19870814_timess; \
 		__19870814_xixi_tag_you_not_used_start = clock(); \
+		__19870814_timess = (times); \
 				\
 		for(__19870814_xixi_tag_you_not_used_i = 0; __19870814_xixi_tag_you_not_used_i < (times); ++__19870814_xixi_tag_you_not_used_i) \
 		{ 
@@ -18,7 +19,7 @@
 #define TIME_END \
 		} \
 		__19870814_xixi_tag_you_not_used_finish = clock(); \
-		printf("Time Elapsed:%5f seconds\n", 1.0 * (__19870814_xixi_tag_you_not_used_finish - __19870814_xixi_tag_you_not_used_start) / CLOCKS_PER_SEC); \
+		printf("Time Elapsed:%5f seconds(Loop Cnt:%d)\n", 1.0 * (__19870814_xixi_tag_you_not_used_finish - __19870814_xixi_tag_you_not_used_start) / CLOCKS_PER_SEC, __19870814_timess); \
 	} 
 #endif
 
